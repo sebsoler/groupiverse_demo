587ab052b006e0cd25e0ebd5bb3673e82da3cee1
sebsoler <sebastian.a.soler@gmail.com>
2011-06-23 12:30:49 -0400

    fixed hp error

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ GroupiverseDemo::Application.routes.draw do
   
   get "users/new"
 
+  match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
